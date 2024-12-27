b9f1d126cbe64bda0041b71260241cbcdf1033b5
Martin Ilgner <martinilgner22@gmail.com>
2024-12-27 20:19:05 +0100

    Fix test and minor grammar issue

--- src/main/scala/miniJavaParser/test.scala
+++ src/main/scala/miniJavaParser/test.scala
@@ -4,10 +4,10 @@ import org.antlr.v4.runtime.{BufferedTokenStream, CharStreams}
 
 @main
 def main(): Unit = {
-  val source = CharStreams.fromString("if (true) return 1;")
+  val source = CharStreams.fromString("class HelloWorld { }")
   val lexer = new miniJavaLexer(source)
   val tokens = new BufferedTokenStream(lexer)
   val parser = new miniJavaParser(tokens)
-  val tree = parser.statement()
+  val tree = parser.compilationUnit()
   print(tree.toStringTree(parser))
 }
