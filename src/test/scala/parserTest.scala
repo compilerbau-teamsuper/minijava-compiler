340f5ef2d4448954b7900a938c3cf3624a589e5c
Luca Dürr <luca.duerr@bosch.com>
2025-02-04 14:46:48 +0100

    Rewrote method test

--- src/test/scala/parserTest.scala
+++ src/test/scala/parserTest.scala
@@ -13,8 +13,13 @@ object ParserTest extends TestSuite {
             ast ==> expected
         }
         test("method parsing") {
-            val ast = JavaASTBuilder.parseFromText("class HelloWorld { private static int plusOne(int x){return x + 1;} }")
-            val expected = CompilationUnit(None,List(),List(ClassDeclaration(List(),"HelloWorld",QualifiedName(List(),"Object"),List(),ClassBody(List(MethodDeclaration(List(Modifier.Private, Modifier.Static), PrimitiveType.Int, "plusOne", List(Parameter("x", PrimitiveType.Int)), Block(List(ReturnStatement(Some(BinaryExpression(QualifiedName(List(), "x"), BinaryOperator.Add, IntLiteral(1))))))))))))
+            val ast = JavaASTBuilder.parseFromFile("src/test/java/methodTest.java")
+            val expected = CompilationUnit(None,List(), List(ClassDeclaration(List(Modifier.Public),"methodTest",QualifiedName(List(),"Object"),List(),
+                ClassBody(List(
+                    MethodDeclaration(List(Modifier.Private, Modifier.Static),PrimitiveType.Int,"plusOne",List(Parameter("x",PrimitiveType.Int)),
+                        Block(List(ReturnStatement(Some(BinaryExpression(QualifiedName(List(),"x"),BinaryOperator.Add,IntLiteral(1))))))),
+                    MethodDeclaration(List(Modifier.Public),VoidType,"doNothing",List(),
+                        Block(List(ReturnStatement(None)))))))))
 
             ast ==> expected
         }
