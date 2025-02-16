8c4da0beb77fca29d0768122e28d74368dc691fe
Martin Ilgner <martinilgner22@gmail.com>
2025-02-16 20:15:59 +0100

    Move bytecodegen, remove AST usage in bytecodegen

---
+++ src/main/scala/miniJavaBytecode/bytecodegen.scala
@@ -0,0 +1,9 @@
+package miniJavaBytecode
+
+import miniJavaAnalysis.IR.*
+import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Opcodes.*
+
+def codeGen(comp: CompilationUnit): Array[Byte] = {
+    return new Array[Byte](0)
+}
